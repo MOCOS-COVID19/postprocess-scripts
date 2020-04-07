86284a029c34ecc77fa94ddf10f796f76876cfa2
Marcin Bodych <bodychmarcin@gmail.com>
2020-04-07 17:18:46 +0200

    added script for computing cumulative stats (number of infected and detected)

--- cumulative_stats/process_range_of_simulations.py
+++ cumulative_stats/process_range_of_simulations.py
@@ -23,15 +23,20 @@ def detected_cases(df_r1):
     return df.sort_values(by='tdetection').tdetection
 
 
+def infected_cases(df_r2):
+    """ This method should be moved to utils """
+    return df_r2.sort_values(by='contraction_time').contraction_time
+
+
 @click.command()
 @click.option('--path', type=click.Path(exists=True))
 @click.option('--zero', type=int, default=133)
 @click.option('--minus', type=int, default=81)
 @click.option('--minus-days', type=int, default=7)
 @click.option('--minus-tolerance', type=float, default=0.1)
-@click.option('--bundle-days', type=int, default=60)
-@click.option('--bundle-prefix', default='')
-def runner(path, zero, minus, minus_days, minus_tolerance, bundle_days, bundle_prefix):
+@click.option('--days', type=int, default=60)
+@click.option('--prefix', default='')
+def runner(path, zero, minus, minus_days, minus_tolerance, days, prefix):
     """
     Calculates how many sample paths are fitting 2-points criteria and saves bundles to files.
 
@@ -39,9 +44,9 @@ def runner(path, zero, minus, minus_days, minus_tolerance, bundle_days, bundle_p
     :param zero: number of detected cases at zero time (synchronization point)
     :param minus: number of detected cases at past time (2-point checkpoint)
     :param minus_days: how many days are between minus day and zero day (e.g. 7)
-    :param minus_tolerance: fraction of "minus" that is tolerated (e.g. 0.1*minus)
-    :param bundle_days: time horizon used for saving forecast bundle files (e.g. 60)
-    :param bundle_prefix: file prefix for storing bundle coordinations (may be blank)
+    :param minus_tolerance: fraction of "minus" that is tolerated (e.g. 0.1*minus), set to 1 or larger number to not filter at all
+    :param days: time horizon after which total number of detected is calculated (e.g. 60)
+    :param prefix: file prefix for storing results (may be blank)
     :return:
     """
 
@@ -54,20 +59,31 @@ def runner(path, zero, minus, minus_days, minus_tolerance, bundle_days, bundle_p
     y_ = []
     tries = 0
     fails = []
+    print('path,detected,infected')
     for sub_ in list_subfolders_with_paths:
         if os.path.basename(sub_).startswith('agg'):
             continue
 
-        tries += 1
         progression_path = os.path.join(sub_, 'output_df_progression_times.csv')
-        df = pd.read_csv(progression_path)
-        detected = detected_cases(df).values
-        if len(detected) <= zero_time:
+        if not os.path.exists(progression_path):
+            continue
+        contractions_path = os.path.join(sub_, 'output_df_potential_contractions.csv')
+        if not os.path.exists(contractions_path):
             continue
 
-        detected = detected - detected[zero_time]
+        tries += 1
+
+        df = pd.read_csv(progression_path, na_values='None')
+        detected = detected_cases(df).values
+        df = pd.read_csv(contractions_path, na_values='None')
+        infected = infected_cases(df).values
+        df = None
+
+        if len(detected) <= zero_time:
+            continue
         t0 = detected[zero_time]
-        filt_detected = detected[detected <= t0 - minus_days]
+        detected = detected - detected[zero_time]
+        filt_detected = detected[detected <= - minus_days]
         if len(filt_detected) == 0:
             continue
 
@@ -78,22 +94,31 @@ def runner(path, zero, minus, minus_days, minus_tolerance, bundle_days, bundle_p
             continue
 
         successes += 1
-        detected = detected[detected >= 0]
-        x = detected[detected <= bundle_days]
-        y = np.arange(zero_time, zero_time + len(x))
-        x_.extend(list(x))
-        y_.extend(list(y))
-        detected = None
+        detected = detected[detected <= days]
+        detected = len(detected)
 
-    print(fails)
-    print(successes)
-    print(tries)
-    bundle_x = os.path.join(d, f'{bundle_prefix}bundle_x.pkl')
-    bundle_y = os.path.join(d, f'{bundle_prefix}bundle_y.pkl')
-    with open(bundle_x, 'wb') as f:
+        infected = infected[infected <= days + t0]
+        infected = len(infected)
+
+        print(f'{sub_},{detected},{infected}')
+        x_.append(detected)
+        y_.append(infected)
+
+    print(f'statistics,detected,infected')
+    print(f'mean,{np.array(x_).mean()},{np.array(y_).mean()}')
+    print(f'std,{np.array(x_).std()},{np.array(y_).std()}')
+    path_detected_pkl = os.path.join(d, f'{prefix}stats_detected.pkl')
+    path_infected_pkl = os.path.join(d, f'{prefix}stats_infected.pkl')
+    with open(path_detected_pkl, 'wb') as f:
         pickle.dump(x_, f)
-    with open(bundle_y, 'wb') as f:
+    with open(path_infected_pkl, 'wb') as f:
         pickle.dump(y_, f)
+    too_small = (1 - minus_tolerance) * minus_time
+    too_large = (1 + minus_tolerance) * minus_time
+    print(f'bundle condition failing values: {fails}, '
+          f'smaller than {too_small:.1f}: {len([fail for fail in fails if fail < too_small])}, '
+          f'larger than {too_large:.1f}: {len([fail for fail in fails if fail > too_large])}')
+    print(f'bundle success ratio: {successes}/{tries}')
 
 
 if __name__ == '__main__':
