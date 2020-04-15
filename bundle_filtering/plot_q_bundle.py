071010e6a11660590d347eeb94b0d7deb7edaea1
Marcin Bodych <marcin.bodych@gds.ey.com>
2020-04-15 21:19:28 +0200

    update to bundle filtering

--- bundle_filtering/plot_q_bundle.py
+++ bundle_filtering/plot_q_bundle.py
@@ -7,7 +7,7 @@ from dateutil import parser
 from scipy import ndimage
 import datetime as dt
 import os
-
+import pandas as pd
 
 def value_to_id(value, max_value, resolution):
     if value < 0:
@@ -33,14 +33,15 @@ def y_to_yid(y, max_y, plot_resolution_y):
 @click.option('--bundle-prefix', default='')
 @click.option('--max-x', type=int, default=60)
 @click.option('--max-y', type=int, default=80000)
+@click.option('--max-y-hospitalized', type=int, default=80000)
+@click.option('--max-y-infections', type=int, default=80000)
 @click.option('--plot-resolution-x', type=int, default=800)
 @click.option('--plot-resolution-y', type=int, default=40000)
-@click.option('--begin-date', default='20200410')
+@click.option('--begin-date', default='20200414')
 @click.option('--sliding-window-length', type=int, default=1)
-def runner(path, simulation_prefix, q_id, outputs_id, bundle_prefix, max_x, max_y, plot_resolution_x, plot_resolution_y,
-           begin_date, sliding_window_length):
+def runner(path, simulation_prefix, q_id, outputs_id, bundle_prefix, max_x, max_y, max_y_hospitalized, max_y_infections,
+           plot_resolution_x, plot_resolution_y, begin_date, sliding_window_length):
     """
-    Calculates how many sample paths are fitting 2-points criteria and saves bundles to files.
 
     :param path: path to set of simulations e.g. "<outputdir>/<experiment_root>/"
     :param simulation_prefix: prefix used for set of simulations, e.g. "grid"
@@ -58,113 +59,160 @@ def runner(path, simulation_prefix, q_id, outputs_id, bundle_prefix, max_x, max_
     :return:
     """
     d = path
-    coeffs_path = os.path.join(d, f'{bundle_prefix}coeffs_{q_id}_{sliding_window_length}.pkl')
-    x__path = os.path.join(d, f'{bundle_prefix}x_{q_id}_{sliding_window_length}.pkl')
-    coeffs_ = []
-    x_ = []
+    detected_path = os.path.join(d, f'{bundle_prefix}detected_{q_id}_{sliding_window_length}.pkl')
+    infected_path = os.path.join(d, f'{bundle_prefix}infected_{q_id}_{sliding_window_length}.pkl')
+    hospitalized_path = os.path.join(d, f'{bundle_prefix}hospitalized_{q_id}_{sliding_window_length}.pkl')
+    reverse_time_path = os.path.join(d, f'{bundle_prefix}x_{q_id}_{sliding_window_length}.pkl')
+    detections_ = []
+    infections_ = []
+    hospitalizations_ = []
+    detections_reverse_time_ = []
     successes = 0
-    if os.path.exists(coeffs_path):
-        with open(coeffs_path, 'rb') as f:
-            coeffs_ = pickle.load(f)
-        if os.path.exists(x__path):
-            with open(x__path, 'rb') as f:
-                x_ = pickle.load(f)
+    if os.path.exists(detected_path):
+        with open(detected_path, 'rb') as f:
+            detections_ = pickle.load(f)
+        if os.path.exists(infected_path):
+            with open(infected_path, 'rb') as f:
+                infections_ = pickle.load(f)
+        if os.path.exists(hospitalized_path):
+            with open(hospitalized_path, 'rb') as f:
+                hospitalizations_ = pickle.load(f)
+        if os.path.exists(reverse_time_path):
+            with open(reverse_time_path, 'rb') as f:
+                detections_reverse_time_ = pickle.load(f)
         successes = 1
     else:
         list_subfolders_with_paths = [f.path for f in os.scandir(d) if f.is_dir()]
         sim_filter = f'{simulation_prefix}_{q_id}_'
         for sub_ in list_subfolders_with_paths:
-            print(f'entering {sub_}')
-            print(f'basename: {os.path.basename(sub_)} vs filter: {sim_filter}')
             if not os.path.basename(sub_).startswith(sim_filter):
                 continue
             #second option as a fallback
             for bundle_dir in [os.path.join(sub_, 'outputs', outputs_id), os.path.join(sub_, outputs_id)]:
-                coeff_path = os.path.join(bundle_dir, f'{bundle_prefix}coeffs_{sliding_window_length}.pkl')
-                if os.path.exists(coeff_path):
-                    with open(coeff_path, 'rb') as f:
-                        coeffs = pickle.load(f)
-                        coeffs_.extend(coeffs)
+                print(f'bundle_dir: {bundle_dir}')
+                detected_path_ = os.path.join(bundle_dir, f'{bundle_prefix}detected_{sliding_window_length}.pkl')
+                if os.path.exists(detected_path_):
+                    with open(detected_path_, 'rb') as f:
+                        detections = pickle.load(f)
+                        detections_.extend(detections)
+                    infected_path_ = os.path.join(bundle_dir, f'{bundle_prefix}infected_{sliding_window_length}.pkl')
+                    hospitalized_path_ = os.path.join(bundle_dir, f'{bundle_prefix}hospitalized_{sliding_window_length}.pkl')
+                    if os.path.exists(infected_path_):
+                        with open(infected_path_, 'rb') as f:
+                            infections = pickle.load(f)
+                            infections_.extend(infections)
+                    if os.path.exists(hospitalized_path_):
+                        with open(hospitalized_path_, 'rb') as f:
+                            hospitalizations = pickle.load(f)
+                            hospitalizations_.extend(hospitalizations)
+
                     successes += 1
-                elif sliding_window_length == 1:
+                elif sliding_window_length == 1: # this is to have backward compatibility for a moment
                     coeff_path = os.path.join(bundle_dir, f'{bundle_prefix}coeffs.pkl')
                     if os.path.exists(coeff_path):
                         with open(coeff_path, 'rb') as f:
                             coeffs = pickle.load(f)
-                            coeffs_.extend(coeffs)
+                            detections_.extend(coeffs)
                         successes += 1
                     else:
                         print(f'cannot read - {coeff_path} do not exist!')
                         continue
                 else:
-                    print(f'cannot read - {coeff_path} do not exist!')
+                    print(f'cannot read - file do not exists!')
                     continue
                 x_path = os.path.join(bundle_dir, f'{bundle_prefix}x_{sliding_window_length}.pkl')
                 if os.path.exists(x_path):
                     with open(x_path, 'rb') as f:
                         x = pickle.load(f)
-                        x_.extend(x)
+                        detections_reverse_time_.extend(x)
+                break
         print(successes)
-        with open(coeffs_path, 'wb') as f:
-            pickle.dump(coeffs_, f)
-        with open(x__path, 'wb') as f:
-            pickle.dump(x_, f)
+        with open(detected_path, 'wb') as f:
+            pickle.dump(detections_, f)
+        with open(infected_path, 'wb') as f:
+            pickle.dump(infections_, f)
+        with open(hospitalized_path, 'wb') as f:
+            pickle.dump(hospitalizations_, f)
+        with open(reverse_time_path, 'wb') as f:
+            pickle.dump(detections_reverse_time_, f)
     if successes > 0:
-        array = np.zeros((plot_resolution_x, plot_resolution_y))
-        x1 = np.arange(max_x * 1000)/1000
-        for coeffs in coeffs_:
-            p = np.poly1d(coeffs)
-            y1 = np.exp(p(x1))
-            zer = np.zeros_like(array)
-            prev_point = None
-            for x_elem, y_elem in zip(x1, y1):
-                if y_elem > max_y:
-                    continue
-                if x_elem > max_x:
-                    continue
-                x_p = x_to_xid(x_elem, max_x, plot_resolution_x)
-                y_p = y_to_yid(y_elem, max_y, plot_resolution_y)
-                if prev_point is None:
-                    zer[x_p, y_p] = 1.0
-                else:
-                    zer[prev_point[0]:(x_p+1), prev_point[1]:(y_p+1)] = 1.0
-                prev_point = (x_p, y_p)
-            array += zer
-        fig, ax = plt.subplots(figsize=(10, 6))
-        s = ndimage.generate_binary_structure(2, 1)
-        array = ndimage.grey_dilation(array, footprint=s)
-        pa = ax.imshow(np.rot90(array), cmap='BuPu', vmin=0, vmax=np.maximum(5, np.percentile(array, 99)), aspect='auto')
-        ax.set_title(f'q={q_id}, ({bundle_prefix.strip("_")})', fontsize=18)
-        cbb = plt.colorbar(pa, shrink=0.35)
-        cbarlabel = 'Zagęszczenie trajektorii'
-        cbb.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom", fontsize=18)
+        def draw(item, maxy, ylabel, filename_fig):
+            array = np.zeros((plot_resolution_x, plot_resolution_y))
+            # x1 = np.arange(max_x * 1000)/1000
+            for detections in item:
+                x1, y1 = zip(*detections)
+                # p = np.poly1d(coeffs)
+                # y1 = np.exp(p(x1))
+                zer = np.zeros_like(array)
+                prev_point = None
+                for x_elem, y_elem in zip(x1, y1):
+                    if y_elem > maxy:
+                        continue
+                    if x_elem > max_x:
+                        continue
+                    x_p = x_to_xid(x_elem, max_x, plot_resolution_x)
+                    y_p = y_to_yid(y_elem, maxy, plot_resolution_y)
+                    if prev_point is None:
+                        zer[x_p, y_p] = 1.0
+                    else:
+                        zer[prev_point[0]:(x_p + 1), prev_point[1]:(y_p + 1)] = 1.0
+                    prev_point = (x_p, y_p)
+                array += zer
+            fig, ax = plt.subplots(figsize=(10, 6))
+            # s = ndimage.generate_binary_structure(2, 1)
+            # array = ndimage.grey_dilation(array, footprint=s)
+            pa = ax.imshow(np.rot90(array), cmap='BuPu', vmin=0, vmax=np.maximum(5, np.percentile(array, 99)),
+                           aspect='auto')
+            ax.set_title(f'q={q_id}, ({bundle_prefix.strip("_")})', fontsize=18)
+            cbb = plt.colorbar(pa, shrink=0.35)
+            cbarlabel = 'Zagęszczenie trajektorii'
+            cbb.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom", fontsize=18)
 
-        ax.set_xticks(np.arange(0, plot_resolution_x + 1, 7 * plot_resolution_x / max_x))
-        now = parser.parse(begin_date)
-        then = now + dt.timedelta(days=max_x + 1)
-        days = mdates.drange(now, then, dt.timedelta(days=7))
-        t = [dt.datetime.fromordinal(int(day)).strftime('%d/%m/%y') for day in days]
-        ax.set_xticklabels([t[i] for i, v in enumerate(range(0, max_x + 1, 7))], rotation=30)
-        ax.set_yticks([v for v in np.arange(plot_resolution_y, -1, -plot_resolution_y / 10.0)])
-        ax.set_yticklabels(
-            [int(v) for v in np.arange(0, max_y + 1, max_y / 10.0)])  # , list(np.arange(20)))
+            ax.set_xticks(np.arange(0, plot_resolution_x + 1, 7 * plot_resolution_x / max_x))
+            now = parser.parse(begin_date)
+            then = now + dt.timedelta(days=max_x + 1)
+            days = mdates.drange(now, then, dt.timedelta(days=7))
+            t = [dt.datetime.fromordinal(int(day)).strftime('%d/%m/%y') for day in days]
+            ax.set_xticklabels([t[i] for i, v in enumerate(range(0, max_x + 1, 7))], rotation=30)
+            ax.set_yticks([v for v in np.arange(plot_resolution_y, -1, -plot_resolution_y / 10.0)])
+            ax.set_yticklabels(
+                [int(v) for v in np.arange(0, maxy + 1, maxy / 10.0)])  # , list(np.arange(20)))
+
+            ax.set_ylabel(ylabel, fontsize=18)
+            xlabel_pl = 'Data'
+            xlabel_en = 'Days from today'
+            xlabel = xlabel_pl
+            ax.set_xlabel(xlabel, fontsize=18, labelpad=16)
+            plt.tight_layout()
+            plt.savefig(os.path.join(d, filename_fig), dpi=300)
+            plt.close(fig)
+
+        draw(detections_, max_y, 'Liczba zdiagnozowanych przypadków', f'bundle_{q_id}_{bundle_prefix}_detections.png')
+        draw(infections_, max_y_infections, 'Liczba zakażonych', f'bundle_{q_id}_{bundle_prefix}_infections.png')
+        draw(hospitalizations_, max_y_hospitalized, 'Liczba hospitalizacji (sumaryczna)', f'bundle_{q_id}_{bundle_prefix}_hospitalizations.png')
+
+        # now draw back in time
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.set_title("Weryfikacja dla poprzednich dni", fontsize=18)
+        for reverse_time in detections_reverse_time_:
+            x, y = zip(*reverse_time)
+            x = [now + dt.timedelta(days=el) for el in x if el <= 0]
+            ax.plot(x, y, 'r-')
+        dat = pd.read_csv('../data/pl_detections.csv',
+                          converters={'date': (lambda x: parser.parse(x, dayfirst=True))})
+        ax.plot('date', 'detected', 'k.', data=dat)
+        ax.format_xdata = mdates.DateFormatter('%d/%m/%y')
+        ax.get_xaxis().set_major_locator(mdates.DayLocator(interval=14))
+        ax.get_xaxis().set_major_formatter(mdates.DateFormatter('%d/%m/%y'))
         ylabel_pl = 'Liczba zdiagnozowanych przypadków'
         ylabel_en = 'detected cases'
         ylabel = ylabel_pl
         xlabel_pl = 'Data'
         xlabel_en = 'Days from today'
         xlabel = xlabel_pl
+
         ax.set_ylabel(ylabel, fontsize=18)
         ax.set_xlabel(xlabel, fontsize=18, labelpad=16)
-        plt.tight_layout()
-        plt.savefig(os.path.join(d, f'bundle_{q_id}_{bundle_prefix}_test.png'), dpi=300)
-        plt.close(fig)
-
-        # now draw back in time
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.set_title("Weryfikacja dla poprzednich dni", fontsize=18)
-        for x in x_:
-            ax.plot(x, np.arange(1, 1 + len(x)), 'r-')
         plt.tight_layout()
         plt.savefig(os.path.join(d, f'check_bundle_{q_id}_{bundle_prefix}_test.png'), dpi=300)
         plt.close(fig)
--- bundle_filtering/process_range_of_simulations.py
+++ bundle_filtering/process_range_of_simulations.py
@@ -23,6 +23,27 @@ def detected_cases(df_r1):
     df = df_r1[cond]
     return df.sort_values(by='tdetection').tdetection
 
+def hospitalized_cases(df_r1):
+    """ This method should be moved to utils """
+    cond1 = ~df_r1.t2.isna()
+    cond2a = ~df_r1.trecovery.isna()
+    cond2b = df_r1.t2 > df_r1.trecovery
+    cond2 = ~np.logical_and(cond2a, cond2b)
+    if len(df_r1[~df_r1.tdeath.isna()]) > 0:
+        cond3a = ~df_r1.tdeath.isna()
+        cond3b = df_r1.t2 > df_r1.tdeath
+        cond3 = ~np.logical_and(cond3a, cond3b)
+        cond23 = np.logical_and(cond2, cond3)
+    else:
+        cond23 = cond2
+    cond = np.logical_and(cond1, cond23)
+    df = df_r1[cond]
+    return df.sort_values(by='t2').t2
+
+def infected_cases(df_r2):
+    """ This method should be moved to utils """
+    return df_r2.sort_values(by='contraction_time').contraction_time
+
 
 def n_days_back(x, ref, n):
     filtered = x[x <= (ref - n)]
@@ -42,11 +63,26 @@ def avg_array(x, n, max_len):
     avg = [n_avg(x, elem, n) for elem in x[:int(max_len)]]
     return np.array(avg)
 
+def every_nth(seq, seq2, step=0.1):
+    assert len(seq) == len(seq2)
+    ret = []
+    ret2 = []
+    while len(seq) > 0:
+        ret.append(seq[0])
+        ret2.append(seq2[0])
+        id = np.argmax(seq >= seq[0] + step)
+        if id == 0:
+            break
+        seq = seq[id:]
+        seq2 = seq2[id:]
+    return np.array(ret), np.array(ret2)
 
+#@click.option('--data', type=click.Path(exists=True))
+#@click.option('--zero-date', default='20200414')
 @click.command()
 @click.option('--path', type=click.Path(exists=True))
-@click.option('--zero', type=float, default=133.0)
-@click.option('--minus', type=float, default=81.0)
+@click.option('--zero', type=float, default=0.0)
+@click.option('--minus', type=float, default=0.0)
 @click.option('--minus2', type=float, default=-1.0)
 @click.option('--minus-days', type=float, default=7.0)
 @click.option('--minus2-days', type=float, default=14.0)
@@ -79,27 +115,33 @@ def runner(path, zero, minus, minus2, minus_days, minus2_days, minus_tolerance, 
     minus_time = minus
     minus2_time = minus2
     successes = 0
-    coeffs = []
     tries = 0
     fails = []
     fails2 = []
-    x_ = []
-    for sub_ in list_subfolders_with_paths:
+    detected_check_ = []
+    hospitalized_ = []
+    infected_ = []
+    detected_ = []
+    arrs = [detected_, hospitalized_, infected_]
+    for i, sub_ in enumerate(list_subfolders_with_paths):
+        print(sub_)
         if os.path.basename(sub_).startswith('agg'):
             continue
-
         progression_path = os.path.join(sub_, 'output_df_progression_times.csv')
         if not os.path.exists(progression_path):
             continue
+        contractions_path = os.path.join(sub_, 'output_df_potential_contractions.csv')
+        if not os.path.exists(contractions_path):
+            continue
 
         tries += 1
+
         df = pd.read_csv(progression_path, na_values='None')
         detected = detected_cases(df).values
-        df = None
 
         if n_avg(detected, detected[-1], sliding_window_length) <= zero_time:
             continue
-        avg_detected = avg_array(detected, sliding_window_length, zero_time*2)
+        avg_detected = avg_array(detected, sliding_window_length, zero_time*1.2)
         zero_time_av = np.argmax(avg_detected[avg_detected <= zero_time])
         t0 = detected[zero_time_av]
         detected = detected - t0
@@ -127,28 +169,49 @@ def runner(path, zero, minus, minus2, minus_days, minus2_days, minus_tolerance, 
                 continue
 
         successes += 1
-        x = detected[detected >= 0]
-        x = x[x <= days]
-        # TODO: if we want to display bundles for averages instead:
-        # y = avg_detected[zero_time_av:zero_time_av + len(x)]
-        y = np.arange(zero_time_av + 1, zero_time_av + 1 + len(x))
-        coeff = np.polyfit(x, np.log(y), 5)
-        coeffs.append(coeff)
-        print(f'{sub_},{coeff}')
+
+        hospitalized = hospitalized_cases(df).values
+        df = pd.read_csv(contractions_path, na_values='None')
+        infected = infected_cases(df).values
+        df = None
+        hospitalized = hospitalized - t0
+        infected = infected - t0
+
+        for i, arr in enumerate([detected, hospitalized, infected]):
+            start_y = np.argmax(arr >= 0) + 1
+            x = arr[arr >= 0]
+            x = x[x <= days]
+            # TODO: if we want to display bundles for averages instead:
+            # y = avg_detected[zero_time_av:zero_time_av + len(x)]
+            y = np.arange(start_y, start_y + len(x))
+            print(f'{len(x)} {len(y)}')
+            x, y = every_nth(np.array(x), y, step=0.05)
+            print(f'{len(x)} {len(y)}')
+            arrs[i].append(zip(x, y))
+        #coeff = np.polyfit(x, np.log(y), 5)
+        #coeffs.append(coeff)
+        #print(f'{sub_},{coeff}')
         x = detected[detected <= 0]
-        x_.append(x)
+        y = np.arange(1, 1 + len(x))
+        print(f'{len(x)} {len(y)}')
+        x, y = every_nth(np.array(x), y, step=0.05)
+        print(f'{len(x)} {len(y)}')
+        detected_check_.append(zip(x, y))
         # TODO: if we want to display bundles for averages instead, we need to store y_ as well
         detected = None
+        infected = None
+        hospitalized = None
         x = None
         y = None
 
-    coeff_path = os.path.join(d, f'{prefix}coeffs_{sliding_window_length}.pkl')
-    with open(coeff_path, 'wb') as f:
-        pickle.dump(coeffs, f)
+    for i, arr_str in enumerate(['detected', 'hospitalized', 'infected']):
+        save_path = os.path.join(d, f'{prefix}{arr_str}_{sliding_window_length}.pkl')
+        with open(save_path, 'wb') as f:
+            pickle.dump(arrs[i], f)
 
     x_path = os.path.join(d, f'{prefix}x_{sliding_window_length}.pkl')
     with open(x_path, 'wb') as f:
-        pickle.dump(x_, f)
+        pickle.dump(detected_check_, f)
 
     too_small = (1 - minus_tolerance) * minus_time
     too_large = (1 + minus_tolerance) * minus_time
