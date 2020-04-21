7c524ccec6f2ad2fb14b8008986e080b42d78e30
Marcin Bodych <marcin.bodych@gds.ey.com>
2020-04-21 02:38:23 +0200

    updated bundle logic for python; updated data

--- bundle_filtering/process_range_of_simulations.py
+++ bundle_filtering/process_range_of_simulations.py
@@ -86,39 +86,28 @@ def every_nth(seq, seq2, step=0.1):
 #@click.option('--zero-date', default='20200414')
 @click.command()
 @click.option('--path', type=click.Path(exists=True))
-@click.option('--zero', type=float, default=0.0)
-@click.option('--minus', type=float, default=0.0)
-@click.option('--minus2', type=float, default=-1.0)
-@click.option('--minus-days', type=float, default=7.0)
-@click.option('--minus2-days', type=float, default=14.0)
-@click.option('--minus-tolerance', type=float, default=0.1)
-@click.option('--minus2-tolerance', type=float, default=0.21)
+@click.option('--offset-days', type=float, default=7.0)
+@click.option('--offset-tolerance', type=float, default=0.05)
 @click.option('--days', type=int, default=60)
 @click.option('--prefix', default='')
 @click.option('--sliding-window-length', type=int, default=1)
-def runner(path, zero, minus, minus2, minus_days, minus2_days, minus_tolerance, minus2_tolerance, days, prefix, sliding_window_length):
+@click.option('--groundtruth-path')
+def runner(path, offset_days, offset_tolerance, days, prefix, sliding_window_length, groundtruth_path):
     """
     Calculates how many sample paths are fitting 2-points criteria and saves bundles to files.
 
     :param path: path to set of simulations e.g. "<outputdir>/<experiment_root>/grid_X_Y/outputs/<outputs_id>/"
-    :param zero: number of detected cases at zero time (synchronization point)
-    :param minus: number of detected cases at past time (2- or 3-point checkpoint)
-    :param minus2: number of detected cases at second past time (3-point checkpoint); if minus2 < 0, we ignore this and use 2-point checkpoint
-    :param minus_days: how many days are between minus day and zero day (e.g. 7)
-    :param minus2_days: how many days are between minus2 day and zero day (e.g. 14)
-    :param minus_tolerance: fraction of "minus" that is tolerated (e.g. 0.1*minus)
-    :param minus2_tolerance: fraction of "minus2" that is tolerated (e.g. 0.21*minus)
+    :param offset_days: how many days are between minus day and zero day (e.g. 7)
+    :param offset_tolerance: fraction of "minus" that is tolerated (e.g. 0.1*minus)
     :param days: time horizon used for saving forecast bundle files (e.g. 60)
     :param prefix: file prefix for storing bundle coordinations (may be blank)
     :param sliding_window_length: if 1, use values for zero and minus, if >1, then use avg over last sliding_window_length days
+    :param groundtruth_path
     :return:
     """
     assert sliding_window_length >= 1
     d = path
     list_subfolders_with_paths = [f.path for f in os.scandir(d) if f.is_dir()]
-    zero_time = zero
-    minus_time = minus
-    minus2_time = minus2
     successes = 0
     tries = 0
     fails = []
@@ -129,6 +118,11 @@ def runner(path, zero, minus, minus2, minus_days, minus2_days, minus_tolerance, 
     hospitalized_current_ = []
     infected_ = []
     detected_ = []
+
+    minus_cases = dw_dets.average4.values[-15]
+    zero_time = dw_dets.average4.values[-8]
+    plus_cases = dw_dets.average4.values[-1]
+
     arrs = [detected_, hospitalized_, infected_, hospitalized_current_]
     for i, sub_ in enumerate(list_subfolders_with_paths):
         print(sub_)
@@ -148,32 +142,31 @@ def runner(path, zero, minus, minus2, minus_days, minus2_days, minus_tolerance, 
 
         if n_avg(detected, detected[-1], sliding_window_length) <= zero_time:
             continue
-        avg_detected = avg_array(detected, sliding_window_length, zero_time*1.2)
+        avg_detected = avg_array(detected, sliding_window_length, plus_cases * 1.4)
         zero_time_av = np.argmax(avg_detected[avg_detected <= zero_time])
         t0 = detected[zero_time_av]
         detected = detected - t0
 
-        filt_detected = detected[detected <= - minus_days]
+        filt_detected = detected[detected <= - offset_days]
         if len(filt_detected) == 0:
             continue
 
+
         arg_tminus = np.argmax(filt_detected)
 
-        if np.abs(avg_detected[arg_tminus] - minus_time) > minus_tolerance * minus_time:
+        if np.abs(avg_detected[arg_tminus] - minus_cases) > offset_tolerance * minus_cases:
             fails.append(avg_detected[arg_tminus])
             continue
 
-        if minus2 > 0.0:
+        filt_detected = detected[detected <= offset_days]
+        if len(filt_detected) == 0:
+            continue
 
-            filt_detected = detected[detected <= - minus2_days]
-            if len(filt_detected) == 0:
-                continue
+        arg_tplus = np.argmax(filt_detected)
 
-            arg_tminus = np.argmax(filt_detected)
-
-            if np.abs(avg_detected[arg_tminus] - minus2_time) > minus2_tolerance * minus2_time:
-                fails2.append(avg_detected[arg_tminus])
-                continue
+        if np.abs(avg_detected[arg_tplus] - plus_cases) > offset_tolerance * plus_cases:
+            fails2.append(avg_detected[arg_tplus])
+            continue
 
         successes += 1
 
@@ -237,17 +230,17 @@ def runner(path, zero, minus, minus2, minus_days, minus2_days, minus_tolerance, 
     with open(x_path, 'wb') as f:
         pickle.dump(detected_check_slide_, f)
 
-    too_small = (1 - minus_tolerance) * minus_time
-    too_large = (1 + minus_tolerance) * minus_time
+    too_small = (1 - offset_tolerance) * minus_cases
+    too_large = (1 + offset_tolerance) * minus_cases
     print(f'bundle condition failing values: {fails}, '
           f'smaller than {too_small:.1f}: {len([fail for fail in fails if fail < too_small])}, '
           f'larger than {too_large:.1f}: {len([fail for fail in fails if fail > too_large])}')
-    if minus2 > 0.0:
-        too_small = (1 - minus2_tolerance) * minus2_time
-        too_large = (1 + minus2_tolerance) * minus2_time
-        print(f'bundle condition failing values: {fails2}, '
-              f'smaller than {too_small:.1f}: {len([fail for fail in fails2 if fail < too_small])}, '
-              f'larger than {too_large:.1f}: {len([fail for fail in fails2 if fail > too_large])}')
+
+    too_small = (1 - offset_tolerance) * plus_cases
+    too_large = (1 + offset_tolerance) * plus_cases
+    print(f'bundle condition failing values: {fails2}, '
+          f'smaller than {too_small:.1f}: {len([fail for fail in fails2 if fail < too_small])}, '
+          f'larger than {too_large:.1f}: {len([fail for fail in fails2 if fail > too_large])}')
 
     print(f'bundle success ratio: {successes}/{tries}')
 
